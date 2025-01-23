d30a42dde0c2249e4e26043888d1db75ce3c282a
Johnny.Wang <johnny.wang@btse.com>
2025-01-23 17:20:50 +0800

    feat: init log4j and quartz

--- src/main/java/com/frizo/daphnis/Main.java
+++ src/main/java/com/frizo/daphnis/Main.java
@@ -1,8 +1,31 @@
 package com.frizo.daphnis;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+@Slf4j
 public class Main {
 
+    @SneakyThrows
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Define the job and tie it to the MyJob class
+        JobDetail job = JobBuilder.newJob(MyJob.class)
+                .withIdentity("myJob", "group1")
+                .build();
+
+        // Trigger the job to run every second using a cron expression
+        Trigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("myTrigger", "group1")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/1 * * * * ?")) // Every second
+                .build();
+
+        // Schedule the job using the scheduler
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        scheduler.scheduleJob(job, trigger);
+
+        log.info("Scheduler started. Press Ctrl+C to stop.");
     }
 }
---
+++ src/main/java/com/frizo/daphnis/MyJob.java
@@ -0,0 +1,15 @@
+package com.frizo.daphnis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+@Slf4j
+public class MyJob implements Job {
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        log.info("Hello, World!");
+    }
+}
